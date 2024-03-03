2694a9a90e2e92895174ec9f59be16ce236c529c
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2024-03-03 18:03:55 +0300

    Разделены сборка и запуск контейнера. Написан скрипт для запуска jupyter notebook в контейнере

--- jupyter.sh
+++ jupyter.sh
@@ -3,8 +3,11 @@
 
 docker run \
 --rm \
--it \
 --mount \
 type=bind,src="$(pwd)",target=/dockeruser/comp_ds_prj \
+-p 127.0.0.1:8080:8080 \
 comp_ds_prj \
-$@
+poetry run jupyter notebook \
+--ip 0.0.0.0 \
+--port 8080 \
+--no-browser
