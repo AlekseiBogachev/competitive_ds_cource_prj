e8df43c6b6d037395bc2f7c924fac6419b0ca56d
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2024-03-17 09:30:26 +0300

    Выполнен исследовательский анализ признаков таблицы car_train.csv по отдельности. Написаны функции для визуализации распределений значений количественных признаков и распределений количества наблюдений по категориям категориальных признаков

--- comp_ds_prj/visualization/__init__.py
+++ comp_ds_prj/visualization/__init__.py
@@ -1,2 +1,9 @@
 # -*- coding: utf-8 -*-
-from comp_ds_prj.visualization.visualize import count_vals, review_data
+from comp_ds_prj.visualization.visualize import (
+    count_vals,
+    explore_cat_feature,
+    explore_num_feature,
+    plot_hbar,
+    plot_num_feature_distibs,
+    review_data,
+)
--- comp_ds_prj/visualization/visualize.py
+++ comp_ds_prj/visualization/visualize.py
@@ -1,9 +1,12 @@
 # -*- coding: utf-8 -*-
 """Визуализации и вывод информации об объектах."""
 
+from typing import Any
+
 import matplotlib.pyplot as plt
 import missingno as msno
 import pandas as pd
+import seaborn as sns
 from IPython.display import display
 
 
@@ -22,6 +25,7 @@ def review_data(df: pd.DataFrame, n_rows: int = 10) -> None:
         Датафрейм с данными.
     n_rows: int
         Количество первых строк, которые будут выведены.
+        Значение по умолчанию 10.
     """
     with pd.option_context(
         "display.max_columns", None, "display.max_rows", None
@@ -92,16 +96,164 @@ def count_vals(df: pd.DataFrame, name: str) -> None:
         Имя признака, количество повторений уникальных значения которого нужно
         посчитать.
     """
-    print(f"Количество повторений значения признака {name}")
-    display(
-        df
-        [[name]]
-        .value_counts()
-        .reset_index()
-        .rename(
-            columns={
-                name: f"Значение признака {name}",
-                0: "Количество повторений"
-            }
+    print(f"Количество повторов уникальных значений признака {name}")
+
+    with pd.option_context(
+        "display.max_columns", None, "display.max_rows", None
+    ):
+        display(
+            df
+            [[name]]
+            .value_counts(dropna=False)
+            .reset_index()
+            .rename(
+                columns={
+                    name: f"Значение признака {name}",
+                    0: "Количество повторений"
+                }
+            )
         )
+
+
+def plot_hbar(
+    data: pd.DataFrame, column_name: str, **kwargs: Any
+) -> None:
+    """Строит горизонтальную столбчатую диаграмму.
+
+    Строит горизонтальную столбчатую диаграмму категориального признака
+    column_name датафрейма data. График отрисовывается с помощью метода
+    pd.DataFrame.plot(**kwargs), поэтому параметры графика можно передать,
+    как именованные аргументы **kwargs.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Датафрейм, содержащий исследуемый признак.
+    column_name : str
+        Имя признака (столбца датафрейма data).
+    **kwargs : Any, optional
+        Параметры графика, корректные для метода pd.DataFrame.plot.
+    """
+    df: pd.Series = (
+        data
+        [column_name]
+        .value_counts(dropna=False)
+        .sort_values()
     )
+
+    df.plot(kind="barh", **kwargs)
+    plt.show()
+
+    return None
+
+
+def explore_cat_feature(
+    data: pd.DataFrame, column_name: str, **kwargs: Any
+) -> None:
+    """Выводит информацию о количестве уникальных занчений признака.
+
+    Функция поочередно вызывает функции count_vals() и plot_hbar() и передаёт
+    им соответствующие аргумерны. Сначала, выводится таблица с количеством
+    повторений каждого уникального значения признака column_name датафрейма data
+    (вызов count_vals()). Затем, строится горизонтальная столбчатая диаграмма
+    категориального признака column_name датафрейма data (вызов plot_hbar()),
+    где график строится с помощью метода pd.DataFrame.plot(**kwargs),
+    поэтому параметры графика можно передать, как именованные аргументы
+    **kwargs.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Датафрейм, содержащий исследуемый признак.
+    column_name : str
+        Имя признака (столбца датафрейма data).
+    **kwargs : Any, optional
+        Параметры графика, корректные для метода pd.DataFrame.plot.
+    """
+    count_vals(data, column_name)
+    print()
+    plot_hbar(data, column_name, **kwargs)
+
+    return None
+
+
+def plot_num_feature_distibs(data: pd.DataFrame, column_name: str) -> None:
+    """Выводит графики, характеризующие распределение значений признака.
+
+    Выводит на экран гистограмму с KDE, диаграмму размаха и ECDF для признака
+    (столбца) с именем column_name из датафрема data.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Датафрейм, содержащий исследуемый признак.
+    column_name : str
+        Имя признака (столбца датафрейма data).
+    """
+    fig, ax = plt.subplot_mosaic(
+        [['hist', 'ecdf'], ['box',  'ecdf']],
+        figsize=(12, 6),
+        gridspec_kw=dict(width_ratios=[2, 1], height_ratios=[2, 1])
+    )
+
+    sns.histplot(
+        data=data,
+        x=column_name,
+        ax=ax['hist'],
+        stat='density',
+        kde=True,
+        bins=30,
+    )
+    sns.ecdfplot(data=data, x=column_name, ax=ax['ecdf'], stat='proportion')
+    sns.boxplot(data=data, x=column_name, ax=ax['box'])
+
+    plt.suptitle(f'Распределение значений признака {column_name}')
+
+    ax['hist'].set_title('Гистограмма и KDE')
+    ax['hist'].set_ylabel('Плотность вероятноти')
+    ax['hist'].set_xlabel('')
+
+    ax['ecdf'].set_title('ECDF')
+    ax['ecdf'].set_ylabel('Доля значений')
+    ax['ecdf'].tick_params(axis='x', rotation=45)
+
+    for axes in ax:
+        ax[axes].grid()
+
+    plt.tight_layout()
+
+    plt.show()
+
+    return None
+
+
+def explore_num_feature(data: pd.DataFrame, column_name: str) -> None:
+    """Выводит информацию о разспределении значений количественного признака.
+
+    Выводит информацию о разспределении значений количественного признака
+    column_name датафрейма data. Сначала выводит на экран информацию о
+    количестве пропусков, затем, выводит описательные статистики, затем выводит
+    графики распределения значений с помощью функции plot_num_feature_distibs():
+    выводит на экран гистограмму с KDE, диаграмму размаха и ECDF.
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Датафрейм, содержащий исследуемый признак.
+    column_name : str
+        Имя признака (столбца датафрейма data).
+    """
+    print(f"Признак {column_name}")
+    na_counts: int = data[column_name].isna().sum()
+
+    print(f"Количество пропусков: {na_counts}")
+    print()
+
+    print("Описательные статистики:")
+    display(
+        data[[column_name]].describe(percentiles=[0.1, 0.25, 0.5, 0.75, 0.9])
+    )
+
+    plot_num_feature_distibs(data, column_name)
+
+    return None
