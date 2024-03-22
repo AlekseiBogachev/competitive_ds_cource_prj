19194582a8bae6b10437b385f4b4c911253bdae3
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2024-03-22 06:45:30 +0300

    Добавлены функции для исследования пар категориальных переменных и исследования количественной переменной в разрезе значений категориальной

--- comp_ds_prj/visualization/__init__.py
+++ comp_ds_prj/visualization/__init__.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from comp_ds_prj.visualization.visualize import (
+    cat_vs_cat_scatter,
     count_vals,
     explore_cat_feature,
+    explore_cat_vs_cat,
     explore_num_feature,
     num_vs_cat_boxplots,
     plot_hbar,
--- comp_ds_prj/visualization/visualize.py
+++ comp_ds_prj/visualization/visualize.py
@@ -1,10 +1,11 @@
 # -*- coding: utf-8 -*-
 """Визуализации и вывод информации об объектах."""
 
-from typing import Any
+from typing import Any, Callable, Dict, List
 
 import matplotlib.pyplot as plt
 import missingno as msno
+import numpy as np
 import pandas as pd
 import seaborn as sns
 from IPython.display import display
@@ -267,6 +268,148 @@ def explore_num_feature(
     return None
 
 
+def cat_vs_cat_scatter(
+    data: pd.DataFrame,
+    x: str,
+    y: str,
+    jitter: float = 0.5,
+    **kwargs: Any,
+) -> None:
+    """Выводит scatterplot с точками для каждого сочетания признаков x и y.
+
+    Выводит scatterplot, в котором каждая точка соответствует сочетанию значений
+    признаков x и y, цвет точки указывает на количество повторений каждого
+    сочетания. Точки случайно разбросаны вокруг центра, соответствующего
+    сочетанию значений x и y, "количество" случайной величины регулирует
+    параметр jitter. Параметры графика по умолчанию, например cmap и alpha
+    можно переопределить, а также добавить новые, передав их по имени
+    (в **kwargs).
+
+    Параметры графика по умолчанию:
+    xlabel=x
+    ylabel=y
+    c='Количество'
+    cmap='winter'
+    alpha=0.4
+    grid=True
+    rot=-90
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Датафрейм, содержащий исследуемые признаки.
+    x : str
+        Категориальный признак, уникальные значения которого будут отложены по
+        горизонтальной оси.
+    y : str
+        Категориальный признак, уникальные значения которого будут отложены по
+        вертикальной оси.
+    jitter : float, optional
+        "Количество" случайной составляющей, которая разбрасывает точки на
+        графике вокруг точки пересечения их категорий, чтобы те не слипались в
+        одну. Значение по умолчанию 0.5.
+    **kwargs :  Any, optional
+        Параметры графика, корректные для метода
+        pd.DataFrame.plot(kind="scatter"). Позволяет переопределить аналогичные
+        параметры, заданные по умолчанию.
+    """
+    plotparams: Dict[str, Any] = dict(
+        xlabel=x,
+        ylabel=y,
+        c='Количество',
+        cmap='winter',
+        alpha=0.4,
+        grid=True,
+        rot=-90,
+    )
+    plotparams.update(**kwargs)
+
+    x_cats: List[str] = sorted(data[x].unique().tolist())
+    y_cats: List[str] = sorted(data[y].unique().tolist())
+
+    add_jitter: Callable[[pd.Series], pd.Series] = lambda x: (
+        x + np.random.uniform(0, jitter, size=x.shape) - jitter / 2
+    )
+
+    x_ids: Dict[str, int] = {cat: i for i, cat in enumerate(x_cats)}
+    y_ids: Dict[str, int] = {cat: i for i, cat in enumerate(y_cats)}
+
+    df: pd.DataFrame = data[[x, y]].copy()
+    df['x_vals'] = add_jitter(df[x].replace(x_ids))
+    df['y_vals'] = add_jitter(df[y].replace(y_ids))
+    df['Количество'] = df.groupby([x, y]).x_vals.transform('count')
+
+    df.plot(
+        kind="scatter",
+        x="x_vals",
+        y="y_vals",
+        **plotparams
+    )
+
+    plt.xticks(list(x_ids.values()))
+    plt.gca().set_xticklabels(x_ids.keys())
+
+    plt.yticks(list(y_ids.values()))
+    plt.gca().set_yticklabels(y_ids.keys())
+
+    plt.show()
+
+    return None
+
+
+def explore_cat_vs_cat(
+    data: pd.DataFrame,
+    x: str,
+    y: str,
+    title: str,
+    **kwargs: Any,
+) -> None:
+    """Выводит информацию о количестве сочетаний признаков x и y.
+
+    Сначала печатает строку, переданную в параметр title. Затем, выводит
+    сводную таблице с количеством повторений каждого уникального сочетания
+    параметро в x и y датафрейма data с помощью pd.crosstab(data[x], data[y]).
+    Затем выводит график (scatterplot), иллюстрирующий данные выведенной
+    таблицы с помощью функции cat_vs_cat_scatter().
+
+    Для того, чтобы переопределить параметры графика по умолчанию можно передать
+    дополнительный аргументы по имени (в **kwargs).
+
+    Parameters
+    ----------
+    data : pd.DataFrame
+        Датафрейм, содержащий исследуемые признаки.
+    x : str
+        Категориальный признак, уникальные значения которого будут отложены по
+        горизонтальной оси.
+    y : str
+        Категориальный признак, уникальные значения которого будут отложены по
+        вертикальной оси.
+    title : str
+        Строка с описанием содержимого сводной таблицы и заголовок графика.
+    **kwargs :  Any, optional
+        Параметры графика, корректные для функции cat_vs_cat_scatter().
+        Позволяет переопределить аналогичные параметры, заданные по умолчанию.
+    """
+    plot_params: Dict[str, Any] = {"title": title}
+    plot_params.update(**kwargs)
+
+    print(plot_params["title"])
+
+    display(pd.crosstab(data[x], data[y]))
+
+    print()
+
+    cat_vs_cat_scatter(
+        data,
+        x=x,
+        y=y,
+        **plot_params,
+    )
+
+    return None
+
+
 def num_vs_cat_boxplots(
     data: pd.DataFrame,
     cat_feature: str,
@@ -280,6 +423,9 @@ def num_vs_cat_boxplots(
     признака {cat_feature} . В функцию можно передать параметры графика,
     корректные для метода pd.DataFrame.boxplot() с помощью именованных
     аргументов **kwargs.
+
+    Параметры графика можно переопределить передав именнованные параметры,
+    корректные для метода pd.DataFrame.boxplot() через **kwargs.
 
     Parameters
     ----------
