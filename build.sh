125a4c97e34190188da50d5ba6226268fa961e7d
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2024-04-07 14:54:06 +0300

    Реализована сборка отдельного контейнера для локальной среды разработки из базового образа, таким образом, чтобы можно было из бегать монтирования папки с проектом с помощью bind-mounts. Dockerfile_dev_env - dockerfile для контейнера с локальной средой разработки

--- build.sh
+++ build.sh
@@ -1,10 +1,20 @@
 #!/bin/bash
+
+echo "Сборка базового образа" | tee ./logs/docker_build.log
+
 docker build \
---build-arg UID=$(id -u) \
---build-arg GID=$(id -g) \
 --build-arg GITUSER="$(git config user.name)" \
 --build-arg GITEMAIL="$(git config user.email)" \
--t comp_ds_prj \
+-t comp_ds_prj_base \
 --progress=plain \
 . \
-2>&1 | tee ./logs/docker_build.log
+2>&1 | tee -a ./logs/docker_build.log
+
+echo "Сборка образа среды разработки" | tee ./logs/docker_build.log
+
+docker build \
+-f Dockerfile_dev_env \
+-t comp_ds_prj_dev \
+--progress=plain \
+. \
+2>&1 | tee -a ./logs/docker_build.log
