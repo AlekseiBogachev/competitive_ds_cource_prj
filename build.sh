aaa43aa22c20e6684b8ef08831e62b66054411ea
Aleksei Bogachev <bogachev.aleksey.m@gmail.com>
2024-04-09 05:45:44 +0300

    Сборка контейнера с локальной средой разработки (Dockerfile_dev_env и build.sh) настроены так, чтобы из контейнера можно было работать с удалённым репозиторием по https, а аутенфикация выполнялась с помощью PAT

--- build.sh
+++ build.sh
@@ -5,6 +5,7 @@ echo "Сборка базового образа" | tee ./logs/docker_build.log
 docker build \
 --build-arg GITUSER="$(git config user.name)" \
 --build-arg GITEMAIL="$(git config user.email)" \
+--build-arg REPO="AlekseiBogachev/competitive_ds_cource_prj.git" \
 -t comp_ds_prj_base \
 --progress=plain \
 . \
@@ -12,9 +13,12 @@ docker build \
 
 echo "Сборка образа среды разработки" | tee ./logs/docker_build.log
 
+BUILDKIT_PROGRESS=plain \
 docker build \
 -f Dockerfile_dev_env \
 -t comp_ds_prj_dev \
 --progress=plain \
+--no-cache \
+--secret id=pat,src=.dev_env_pat \
 . \
 2>&1 | tee -a ./logs/docker_build.log
